37de128ed4f07a2dc838ae7c150a26999d5e6cf4
KDawgHonda777 <78769730+KDawgHonda777@users.noreply.github.com>
2022-03-20 20:06:52 -0700

    Create random-super-john.js

---
+++ random-super-john.js
@@ -0,0 +1,48 @@
+/*
+ * Nightbot command:
+ * !editcom -ul=everyone -cd=30 !sports $(eval randomCar(); $(urlfetch json https://raw.githubusercontent.com/KDawgHonda777/KDawgHonda/main/random-sports-john.js);)
+ */
+function randomCar() {
+  var cars = [
+    "Weaponized Ingus",
+    "Cyclone II",
+    "Emerus",
+    "Krieger",
+    "Vagner", 
+    "S80RR", 
+    "Thrax", 
+    "Ingus", 
+    "RE-7B", 
+    "Autarch", 
+    "Deveste Eight", 
+    "Tempesta", 
+    "Zentorno", 
+    "Nero Custom", 
+    "Visione", 
+    "X80 Proto", 
+    "ETR1", 
+    "Tezeract", 
+    "T20", 
+    "Tyrant", 
+    "Osiris", 
+    "Zeno", 
+    "Tyrus", 
+    "Nero", 
+    "FMJ", 
+    "Cyclone", 
+    "Turismo R", 
+    "Entity XF", 
+    "Reaper", 
+    "Itali GTB Custom", 
+    "Infernus", 
+    "811", 
+    "Entity XXR", 
+    "Vacca", 
+    "Sultan RS", 
+    "Banshee 900R", 
+    "Adder", 
+    "Voltic"
+  ];
+  var i = Math.floor(Math.random() * cars.length);
+  return cars[i];
+}
