40bc9b40eb3e6a77850a71f17d4aa86aa9201c48
KDawgHonda777 <78769730+KDawgHonda777@users.noreply.github.com>
2025-09-25 15:36:37 -0700

    Create random-offroad.js

---
+++ random-offroad.js
@@ -0,0 +1,46 @@
+/*
+ * Nightbot command:
+ * !editcom -ul=everyone -cd=30 !off-road $(eval randomCar(); $(urlfetch json https://raw.githubusercontent.com/KDawgHonda777/KDawgHonda/main/random-offroad.js);)
+ */
+function randomCar() {
+  var cars = [
+    "Mo", 
+    "Firebolt ASP", 
+    "Draugur", 
+    "Vagrant", 
+    "Terminus", 
+    "Kamacho", 
+    "Street Blazer", 
+    "Ratel", 
+    "Desert Raid", 
+    "Trophy Truck", 
+    "Nightshark", 
+    "Blazer", 
+    "Bifta", 
+    "Dune Buggy", 
+    "Brawler", 
+    "Walton L35", 
+    "Caracara 4x4", 
+    "Yosemite 1500", 
+    "Outlaw", 
+    "Hellion", 
+    "Boor", 
+    "Yosemite Rancher", 
+    "Insurgent", 
+    "Injection", 
+    "Dune", 
+    "Dubsta 6x6", 
+    "Merryweather Mesa", 
+    "Patriot Mil-Spec", 
+    "Rusty Rebel", 
+    "Marshall", 
+    "Liberator", 
+    "Sandking SWB", 
+    "Kalahari", 
+    "Verus", 
+    "Rancher XL", 
+    "Duneloader"
+  ];
+  var i = Math.floor(Math.random() * cars.length);
+  return cars[i];
+}
