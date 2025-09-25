95b964d80368a4c9aa9699772c13596da3424646
KDawgHonda777 <78769730+KDawgHonda777@users.noreply.github.com>
2025-09-25 14:47:44 -0700

    Update random-sportsclassics.js

--- random-sportsclassics.js
+++ random-sportsclassics.js
@@ -1,1 +1,40 @@
-
+/*
+ * Nightbot command:
+ * !editcom -ul=everyone -cd=30 !sports $(eval randomCar(); $(urlfetch json https://raw.githubusercontent.com/KDawgHonda777/KDawgHonda/main/random-sports.js);)
+ */
+function randomCar() {
+  var cars = [
+    "Turismo Classic", 
+    "Stirling GT", 
+    "Uranus LozSpeed", 
+    "LSCM Cheetah Classic", 
+    "Cheetah Classic", 
+    "Ardent", 
+    "Rapid GT Classic", 
+    "Pathetic Easy Mode Mk II", 
+    "Infernus Classic", 
+    "Torero", 
+    "Viseris", 
+    "Zion Classic", 
+    "Savestra", 
+    "Retinue", 
+    "Z-Type", 
+    "Pigalle", 
+    "190z", 
+    "Monroe", 
+    "Coquette Classic", 
+    "Cheburek", 
+    "Mamba", 
+    "Michelli GT", 
+    "Stinger GT", 
+    "Roosevelt Valor", 
+    "Fränken Stange", 
+    "Roosevelt", 
+    "Fagaloa", 
+    "Tornado Custom", 
+    "Peyote", 
+    "Mariachi Tornado"
+  ];
+  var i = Math.floor(Math.random() * cars.length);
+  return cars[i];
+}
